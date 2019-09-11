7a266daa40295ab268d651d8d1ebedae0cc7b3a4
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2019-09-11 11:20:57 -0400

    UPDATES

--- up.sh
+++ up.sh
@@ -3,7 +3,7 @@ rm -rf _build
 rm ./data-structs.tar.gz
 make html
 cd _build/html
-sed -i 's/max-width: 800px;/max-width: 75%;/' _static/basic.css
+sed -i 's/max-width: 800px;/max-width: 65%;/' _static/basic.css
 tar -czvf ../../data-structs.tar.gz  ./*
 cd ../..
 sshpass -pkk0457 scp ./data-structs.tar.gz kurt@23.254.165.183:~/
